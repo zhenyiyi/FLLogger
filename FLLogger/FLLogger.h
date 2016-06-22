46a837d7f7d7aec2077e98d75bf0178f7fcd6b02
zhenyiyi <zhangwenhua@cheyaoshi.com>
2016-06-22 14:59:30 +0800

    add pod files

---
+++ FLLogger/FLLogger.h
@@ -0,0 +1,13 @@
+//
+//  FLLogger.h
+//  CKLogger
+//
+//  Created by fenglin on 6/22/16.
+//  Copyright © 2016 fenglin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLLogger : NSObject
+-(void)test;
+@end
---
+++ FLLogger/FLLogger.m
@@ -0,0 +1,15 @@
+//
+//  FLLogger.m
+//  CKLogger
+//
+//  Created by fenglin on 6/22/16.
+//  Copyright © 2016 fenglin. All rights reserved.
+//
+
+#import "FLLogger.h"
+
+@implementation FLLogger
+-(void)test{
+    
+}
+@end
